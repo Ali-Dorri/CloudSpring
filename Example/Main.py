118cc6d13cd4e79d5c44dde1c0befcf497784a9b
Amir <a.m.sh.playstore@gmail.com>
2020-01-27 10:58:15 +0330

    Add Add_user_Service method and triggers

--- Example/Main.py
+++ Example/Main.py
@@ -46,8 +46,10 @@ Create_Tables_Commands=(
         ,
         """
         CREATE TABLE user_service_info(
-        order_id_pk INTEGER ,
+        order_id_pk SERIAL INTEGER ,
+        user_id_fk INTEGER ,
         PRIMARY KEY(order_id_pk),
+        FOREIGN KEY (user_id_fk) REFERENCES users(user_id) ,
         created_date DATE ,
         end_date DATE NULL ,
         ssh_key VARCHAR(800) ,
@@ -57,13 +59,11 @@ Create_Tables_Commands=(
         ,
         """
         CREATE TABLE user_services(
-        user_id_fk INTEGER ,
-        FOREIGN KEY (user_id_fk) REFERENCES users(user_id) ,
         service_id_fk INTEGER ,
         FOREIGN KEY (service_id_fk) REFERENCES services(service_id) ,
         order_id INTEGER ,
         FOREIGN KEY (order_id) REFERENCES user_service_info(order_id_pk) ,
-        PRIMARY KEY(user_id_fk,service_id_fk,order_id)
+        PRIMARY KEY(service_id_fk,order_id)
         )
         """
         ,
@@ -99,10 +99,10 @@ def connecttodb():
             print('Connected to PostgreSql DB !')
             #sqlservercommand = Create_Tables_Commands
             #for command in sqlservercommand:
-                #cursor.execute(command)
+             #   cursor.execute(command)
 
             #con.commit()
-
+            #print("Commands Executed Successfully")
             #insert = "INSERT INTO resources (resource_id,resource_name) VALUES (2,'OS')"
             #cursor.execute(insert)
             #cursor.close()
@@ -154,21 +154,93 @@ class Ui_MainWindow(object):
     def firstbutton(self):
         print ("Hello-World Amir")
 
+
+def AddUser_Service_Info():
+        queries = ("""INSERT INTO user_service_info (order_id_pk, user_id_fk,created_date,end_date,ssh_key,ssh_name) VALUES (1, 1,'20120315','20130426','7753sdfxcv','name_1')""",
+                 """INSERT INTO user_service_info (order_id_pk, user_id_fk,created_date,end_date,ssh_key,ssh_name) VALUES (2, 2,'20130415','20140315','4512asxczx48','name_2')""",
+                 """INSERT INTO user_service_info (order_id_pk, user_id_fk,created_date,end_date,ssh_key,ssh_name) VALUES (3, 1,'20120705','20140517','xzxc155a54','name_3')""")
+        global Connection
+        for qeury in queries:
+            Connection.cursor().execute(qeury)
+            Connection.commit()
+            print("resources successfully added")
+
+
+def AddResources():
+    queries = ("""INSERT INTO resources (resource_name) VALUES ('OS')""",
+               """INSERT INTO resources (resource_name) VALUES ('RAM')""",
+               """INSERT INTO resources (resource_name) VALUES ('CPU FREQUENCY')""",
+               """INSERT INTO resources (resource_name) VALUES ('HD')""",
+               """INSERT INTO resources (resource_name) VALUES ('BANDWIDTH')""",
+               """INSERT INTO resources (resource_name) VALUES ('CPU CORE')""")
+    global Connection
+    for qeury in queries:
+        Connection.cursor().execute(qeury)
+        Connection.commit()
+        print("resources successfully added")
+
+def DELETEALLRESOIRCE():
+    query = "DELETE FROM resources WHERE resource_id > 0"
+    global Connection
+    try:
+        Connection.cursor().execute(query)
+        Connection.commit()
+        print("All Resources Deleted Successfuly")
+    except(Exception) as error:
+        print(error)
+
+def AddServices():
+    queries = ("""INSERT INTO resources (resource_name) VALUES ('OS')""",
+               """INSERT INTO resources (resource_name) VALUES ('RAM')""",
+               """INSERT INTO resources (resource_name) VALUES ('CPU FREQUENCY')""",
+               """INSERT INTO resources (resource_name) VALUES ('HD')""",
+               """INSERT INTO resources (resource_name) VALUES ('BANDWIDTH')""",
+               """INSERT INTO resources (resource_name) VALUES ('CPU CORE')""")
+    global Connection
+    for qeury in queries:
+        Connection.cursor().execute(qeury)
+        Connection.commit()
+        print("resources successfully added")
+
+
+
+def AddUserEmailTriger():
+    query = """CREATE  TRIGGER  CheckUserEmail
+	    BEFORE INSERT ON users 
+	    REFERENCING NEW ROW AS NewTuple
+	    FOR EACH ROW
+	    WHEN (NewTuple.user_email NOT IN (SELECT user_email FROM users))
+	    INSERT INTO users (first_name,last_name,user_national_code,user_email,user_passwordhash,user_salt,user_registerdate,balance) VALUES (NewTuple.first_name,NewTuple.last_name,NewTuple.user_national_code,NewTuple.user_email,NewTuple.user_passwordhash,NewTuple.user_salt,NewTuple.user_registerdate,NewTuple.balance);"""
+    global Connection
+    try:
+        Connection.cursor().execute(query)
+        Connection.commit()
+        print("Trigger Added Successfully")
+    except(Exception) as error:
+        print(error)
     
 if __name__ == "__main__":
     import sys
     connecttodb()
-    Repo = Repository.Repository(Connection)
-    user = DBType.User('hossein','sharifian','045874425','hossein@yahoo.com','hossein1234','f1nd1ngn3m0','20120618','25000')
-    ticket = DBType.Ticket('1',None,'the content of ticket',None,None,'1')
+    #AddUser_Service_Info()
+    #Repo = Repository.Repository(Connection)
+    #user = DBType.User('hossein','sharifian','045874425','hossein@yahoo.com','hossein1234','f1nd1ngn3m0','20120618','25000')
+    user = DBType.User('Amir', 'sharifian', '045887425', 'Amir@yahoo.com', 'Amir1234', 'f1nd1ngn3m0','20120618', '25000')
+    #user = DBType.User('mahdi', 'kazemi', '045887425', 'asf@yahoo.com', 'kazemi1234', 'f1nd1ngn3m0', '20120618','25000')
+    #ticket = DBType.Ticket('1',None,'the content of ticket',None,None,'1')
+    AddUserEmailTriger()
     #Connection.cursor.execute()
     #result = Repository.RepositoryResult()
     #result = Repo.AddUser(user)
+    #result = Repo.GetCurrentUserServices(user)
     #result = Repo.CheckUser(user.email,"hossein1234")
+    #Repo.GetAvailableOs()
+    #services_to_add = [2,4,7,8,12,13]
+    #result = Repo.AddUserService(services_to_add,user.email,"gdf54g54dfgdg6","sample_ssh_name")
     #result = Repo.AddTicket(ticket)
     #result = Repo.UpdateUserByEmail("85000","asf@yahoo.com")
-    result = Repo.GetUser("asf@yahoo.com")
-    print(result)
+    #result = Repo.GetUser("asf@yahoo.com")
+    #print(result)
 
     """app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
--- Example/Repository.py
+++ Example/Repository.py
@@ -30,7 +30,7 @@ class Repository:
             passwordhash = hashlib.md5(temp.encode())
             print("Final Password hash is : %s"%(passwordhash.hexdigest()))
             finalpasswordhash = passwordhash.hexdigest()
-            query = """INSERT INTO USERS (first_name,last_name,user_national_code,user_email,user_passwordhash,user_salt,user_registerdate,balance) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s');"""\
+            query = """INSERT INTO users (first_name,last_name,user_national_code,user_email,user_passwordhash,user_salt,user_registerdate,balance) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s');"""\
                     % (User.first_name,User.last_name,User.national_code,User.email,finalpasswordhash,User.salt,User.registrationdate,User.balance)
             #Query = "INSERT INTO users(user_name,user_national_code,user_email,user_passwordhash,user_salt,user_registerdate) VALUES(' " + User.user_name + "',' " + User.user_national_code + " ', ' " + User.user_email + " ' ,' " + User.user_passwordhash + " ' , ' " + User.user_salt + " ' ,'" + str(datetimenow) + "')"
             print(query)
@@ -141,6 +141,103 @@ class Repository:
 
 
 
+    def GetUserId(self,user_email):
+        if(user_email is not None):
+            getuseridquery = """SELECT user_id FROM users U WHERE U.user_email = '%s'""" % (user_email)
+            print(getuseridquery)
+            try:
+                self.Cursor.execute(getuseridquery)
+                result = self.Cursor.fetchone()[0]
+                return result
+            except (Exception) as error:
+                print(error)
+                return False
+
+
+    def GetUserPacket(self,user_id):
+        if(user_id is not None):
+            query = """SELECT order_id_pk FROM user_service_info U WHERE U.user_id_fk = '%s'"""%(user_id)
+            print(query)
+            try:
+                self.Cursor.execute(query)
+                result = self.Cursor.fetchall()
+                return result
+            except (Exception) as error:
+                print(error)
+                return False
+
+
+    def GetCurrentUserServices(self,User):
+        if(User.email is not None):
+            result = self.GetUserId(User.email)
+            print(result)
+            packtids = None
+            for user_id in result:
+                packtids = self.GetUserPacket(user_id)[0]
+                print(packtids)
+                if(packtids != []):
+                    query = """SELECT service_id_fk FROM user_services U WHERE U.order_id = '%s' ORDER BY service_id_fk"""%(packtids)
+                    print(query)
+                    try:
+                        self.Cursor.execute(query)
+                        result = self.Cursor.fetchall()
+                        print(result)
+                    except (Exception) as error:
+                        print(error)
+                        return False
+
+
+
+    def GetCurrentUserPackt(self,user_email):
+        if(user_email is not None):
+            result = self.GetUserId(User.email)
+            print(result)
+            packtids = self.GetUserPacket(result)
+            return packtids
+
+    "to add couple of new services with new packet_id for given user"
+    def AddUserService(self,services,user_email,ssh_key,ssh_name):
+        if(services is not None):
+            user_id = self.GetUserId(user_email)
+            query = """SELECT MAX(order_id_pk) FROM user_service_info """
+            print(query)
+            try:
+                self.Cursor.execute(query)
+                maximumorder_id = self.Cursor.fetchone()[0]
+                maximumorder_id = maximumorder_id +1
+                print(maximumorder_id)
+                datetimenow = datetime.now().date()
+                user_id = self.GetUserId(user_email)
+                adduserpacketquery = """INSERT INTO user_service_info (order_id_pk, user_id_fk,created_date,end_date,ssh_key,ssh_name) VALUES ('%s','%s','%s','%s','%s','%s')"""\
+                %(maximumorder_id,user_id,datetimenow,datetimenow,ssh_key,ssh_name)
+                print(adduserpacketquery)
+                self.Cursor.execute(adduserpacketquery)
+                self.DBConnection.commit()
+                for service in services:
+                    add_services_to_packet_query = """INSERT INTO user_services (service_id_fk,order_id) VALUES ('%s','%s')"""%(service,maximumorder_id)
+                    self.Cursor.execute(add_services_to_packet_query)
+                self.DBConnection.commit()
+            except (Exception) as error:
+                print(error)
+                return False
+
+    def DeleteUserService(self):
+
+
+    def GetAvailableResources(self,Resource_id):
+        if(Resource_id is not None):
+            query = """SELECT service_id FROM services S WHERE S.resource_id_fk = '%s' and S.stock > 1"""%(Resource_id)
+            try:
+                self.Cursor.execute(query)
+                result = self.Cursor.fetchall()
+                print(result)
+                return result
+            except (Exception) as error:
+                print(error)
+                return False
+
+
+
     def AddService(self,Service):
         if(Service.service_name is not None & Service.resource_id is not None & Service.stock >= 1):
             query = """INSERT INTO services(service_name,resource_id_fk,stock) VALUES('%s','%s','%s')"""%(Service.service_name,Service.resource_id,Service.stock)
@@ -155,9 +252,12 @@ class Repository:
 
 
 
-    def AddUser_Service(self,User,Service,Packet_Number):
-        if ( Service.service_id is not None & Service.service_name is not None & Service.resource_id is not None & Service.stock >= 1 & User.user_id is not None):
-            Query =  "INSERT INTO user_services(user_id_fk,service_id_fk,resource_id_fk,created_date,end_date,packet_id) VALUES(" + User.user_id  + " , " + Service.service_id + " , " + Service.resource_id + ")"
+
+
+
+    #def AddUser_Service(self,User,Service,Packet_Number):
+     #   if ( Service.service_id is not None & Service.service_name is not None & Service.resource_id is not None & Service.stock >= 1 & User.user_id is not None):
+      #      Query =  "INSERT INTO user_services(user_id_fk,service_id_fk,resource_id_fk,created_date,end_date,packet_id) VALUES(" + User.user_id  + " , " + Service.service_id + " , " + Service.resource_id + ")"
 
 
 
